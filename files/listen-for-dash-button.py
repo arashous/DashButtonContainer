0121705184066a30cc9fc89c4ed695652ba94bb0
arashous <arashous@users.noreply.github.com>
2016-08-06 19:28:25 -0700

    add timestamp to output

--- files/listen-for-dash-button.py
+++ files/listen-for-dash-button.py
@@ -1,9 +1,9 @@
 
 from scapy.all import *
+from time import strftime
 
 def arp_display(pkt):
   if pkt[ARP].op == 1: #who-has (request)
-    print "ARP Probe from device: " + pkt[ARP].hwsrc
+    print "{0} - ARP Probe from device: {1}".format(strftime("%Y%m%d-%H%M%S"), pkt[ARP].hwsrc)
 
 print sniff(prn=arp_display, filter="arp", store=0, count=10)
-
